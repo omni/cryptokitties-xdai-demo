8eb65654fe20a39d96a50a233d2413ed435041ef
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-09-20 16:03:58 -0300

    Add mintable burnable kitty contract

---
+++ contracts/simpleKitty/SimpleBridgeKitty.sol
@@ -0,0 +1,46 @@
+pragma solidity 0.4.24;
+
+import "./SimpleKittyCore.sol";
+import "../kitty/Ownable.sol";
+
+contract SimpleBridgeKitty is Ownable, SimpleKittyCore {
+    function Mint(
+        uint256 _tokenId,
+        bool _isReady,
+        uint256 _cooldownIndex,
+        uint256 _nextActionAt,
+        uint256 _siringWithId,
+        uint256 _birthTime,
+        uint256 _matronId,
+        uint256 _sireId,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) external onlyOwner {
+        _createKitty(
+            _tokenId,
+            _isReady,
+            _cooldownIndex,
+            _nextActionAt,
+            _siringWithId,
+            _birthTime,
+            _matronId,
+            _sireId,
+            _generation,
+            _genes,
+            _owner
+        );
+    }
+
+    function Burn(uint256 _tokenId) external onlyOwner {
+        require(_owns(msg.sender, _tokenId));
+        // remove kitty
+        delete kitties[_tokenId];
+        // reduce total supply
+        kittyTotalSupply--;
+        // remove ownership of the kitty
+        delete kittyIndexToOwner[_tokenId];
+        // reduce owner token account
+        ownershipTokenCount[msg.sender]--;
+    }
+}
